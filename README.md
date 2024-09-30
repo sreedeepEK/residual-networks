60647755f3341a281a0d9f3ee44c9e5dce28180d
Sreedeep . <sreedeepek.95@gmail.com>
2024-10-01 01:24:15 +0530

    update files

---
+++ README.md
@@ -0,0 +1,2 @@
+Implementation of "Deep Residual Learning for Image Recognition" from scratch in pure Pytorch.                        
+Note: This repository is a project under development. 
---
+++ resnet.py
@@ -0,0 +1,32 @@
+import torch 
+import torch.nn as nn 
+
+
+#creating ResidualBlock 
+class ResidualBlock(nn.Module):
+    def __init__(self,in_channels:int,out_channels:int,stride=1):
+        super(ResidualBlock,self).__init__() 
+        
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels,out_channels=out_channels,stride=stride,kernel_size=3,padding=1), 
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU())
+        
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=out_channels,out_channels=out_channels,stride=stride,kernel_size=3,padding=1), 
+            nn.BatchNorm2d(out_channels))
+
+        
+        self.shortcut = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+            nn.BatchNorm2d(out_channels)
+            )
+                    
+    
+    def forward(self,x):
+        residual = x
+        out = self.conv1(x)
+        out = self.conv2(out) 
+        
+        
+        pass 
